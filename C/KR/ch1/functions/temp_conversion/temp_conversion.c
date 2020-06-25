dd63b22dc7d98069bafd95cd281e885866cf2f44
fmgornick <fletchermgornick@gmail.com>
2020-06-24 23:16:33 -0500

    fix temp conversion function

--- C/KR/ch1/functions/temp_conversion/temp_conversion.c
+++ C/KR/ch1/functions/temp_conversion/temp_conversion.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-int conversion(int fahr);
+float conversion(int fahr);
 
 int main()
 {
     int i;
 
     for (i = 0; i <= 300; i += 20) 
-        printf("\n%d°F = %.2f°C", i, conversion(i));
+        printf("\n%3d°F = %6.2f°C", i, conversion(i));
 
     return 0;
 }
 
-int conversion(int fahr)
+float conversion(int fahr)
 {
-    float celsius;
-    celsius = (5.0/9.0) * (fahr - 32);
-
-    return celsius;
+    return (5.0/9.0) * (fahr - 32);
 }
