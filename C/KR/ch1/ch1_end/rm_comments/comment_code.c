94b332721d883b819954696d8a89bb8cb3d05569
fmgornick <fletchermgornick@gmail.com>
2020-07-15 23:09:24 -0500

    removing comments code

---
+++ C/KR/ch1/ch1_end/rm_comments/comment_code.c
@@ -0,0 +1,8 @@
+code code code code // this is a comment
+code code code code code code code code /* this is another comment
+                                           fkasjhfdkdskfdsahfkjhsad 
+                                           askjdhfkljhasdf 
+                                           asdkjfhlkjasdhflkjhsadfjkl*/
+code code code code code code code code 
+code code code code code code code code 
+code code code code code code code code     // oh yeahhh
--- C/KR/ch1/ch1_end/rm_comments/rm_comments.c
+++ C/KR/ch1/ch1_end/rm_comments/rm_comments.c
@@ -1,1 +1,54 @@
 #include <stdio.h>
+
+int main()
+{
+    int c, comment = 0, num_f_slash = 0;
+    FILE *program = fopen("comment_code.c", "r");
+
+    while ((c = fgetc(program)) != EOF) 
+    {
+
+        if (comment == 0 && c == '/' && num_f_slash == 1) {
+            num_f_slash = 0;
+            comment = 1;
+        }
+
+        else if (comment == 0 && c == '/' && num_f_slash == 0) {
+            ++num_f_slash;
+        }
+
+        else if (comment == 0 && c == '*' && num_f_slash == 1) {
+            num_f_slash = 0;
+            comment = 2;
+        }
+
+        else if (comment == 0 && c != '/' && num_f_slash == 1) {
+            putchar('/');
+            putchar(c);
+        }
+
+        else if (comment == 0 && c != '/') {
+            putchar(c);
+        }
+
+        else if (comment == 1 && c == '\n') {
+            comment = 0;
+            num_f_slash = 0;
+            putchar(c);
+        }
+
+        else if (comment == 2 && c == '\n') {
+            putchar(c);
+        }
+
+        else if (comment == 2 && c == '*' && num_f_slash == 0) {
+            ++num_f_slash;
+        }
+
+        else if (comment == 2 && c == '/' && num_f_slash == 1) {
+            num_f_slash = 0;
+            comment = 0;
+        }
+    }
+    return 0;
+}
