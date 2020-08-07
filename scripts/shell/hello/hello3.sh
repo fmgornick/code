d8d14f5619ea535143bda112b45f0523f4da6a0e
fmgornick <fletchermgornick@gmail.com>
2020-08-07 16:42:21 -0500

    make hello3 with less lines

--- scripts/shell/hello/hello3.sh
+++ scripts/shell/hello/hello3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "yo it's the $(basename $0) script, what's your name? \c"
-read
+read -p "yo it's the $(basename $0) script, what's your name? " name
+echo "oh word, what's brackin $name?"
 
-echo "oh word, what's brackin $REPLY?"
+exit 0
