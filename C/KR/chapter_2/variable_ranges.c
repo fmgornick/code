25b8bd21e9f5546c842dce3ff78350ecfee683e0
fmgornick <fletchermgornick@gmail.com>
2020-08-28 16:46:11 -0500

    fixed variable range code

--- C/KR/chapter_2/variable_ranges.c
+++ C/KR/chapter_2/variable_ranges.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <limits.h>
-#include <float.h>
 
 int main()
 {
     printf("range for signed char: %d to %d\n", SCHAR_MIN, SCHAR_MAX);
-    printf("range for unsigned char: %d to %d\n\n", CHAR_MIN, UCHAR_MAX);
+    printf("range for unsigned char: %d to %d\n\n", 0, UCHAR_MAX);
 
     printf("range for signed int: %d to %d\n", INT_MIN, INT_MAX);
-    printf("range for unsigned int: %d to %u\n\n", INT_MIN, UINT_MAX);
+    printf("range for unsigned int: %d to %u\n\n", 0, UINT_MAX);
 
     printf("range for signed short: %d to %d\n", SHRT_MIN, SHRT_MAX);
-    printf("range for unsigned short: %d to %d\n\n", SHRT_MIN, USHRT_MAX);
+    printf("range for unsigned short: %d to %d\n\n", 0, USHRT_MAX);
 
     printf("range for signed long: %ld to %ld\n", LONG_MIN, LONG_MAX);
-    printf("range for unsigned long: %ld to %lu\n\n", LONG_MIN, ULONG_MAX);
+    printf("range for unsigned long: %d to %lu\n\n", 0, ULONG_MAX);
 }
