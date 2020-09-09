28d90929b4e872e668b964afb80fc0903d937c1c
fmgornick <fletchermgornick@gmail.com>
2020-09-08 21:29:16 -0500

    new java helloworlds

---
+++ java/helloworld/H1.java
@@ -0,0 +1,14 @@
+// H1.java
+// first hello world program
+
+// difference between print() and println()
+// println() adds '\n'
+
+public class H1 {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println(", Hello World!");
+        System.out.println("Hello World!");
+    }
+}
---
+++ java/helloworld/H2.java
@@ -0,0 +1,9 @@
+// hello world function using variable in place of string
+
+public class H2 {
+
+    public static void main(String[] args) {
+        String a = "Hello World!";
+        System.out.println(a);
+    }
+}
