b0154e6c9f41aa9a9b216306a62945dd90335933
fmgornick <fletchermgornick@gmail.com>
2020-09-22 10:13:48 -0500

    finished project 1

--- java/csci1933/projects/project1/FractalDrawer.java
+++ java/csci1933/projects/project1/FractalDrawer.java
@@ -17,14 +17,11 @@ public class FractalDrawer {
         Canvas myCanvas = new Canvas(1000,1200);
 
         switch (type) {
-            case "triangle": drawTriangleFractal(1200, 1000, 0, 1000, Color.blue, myCanvas, 8, 0); 
-                             break;
+            case "triangle": return drawTriangleFractal(1200, 1000, 0, 1000, Color.blue, myCanvas, 8, 0, 0); 
 
-            case "circle": drawCircleFractal(200, 400, 400, Color.blue, myCanvas, 10);
-                           break;
+            case "circle": return drawCircleFractal(450, 600, 500, Color.blue, myCanvas, 8, 0, 0);
 
-            case "rectangle": drawRectangleFractal(200, 160, 200, 360, Color.blue, myCanvas, 10);
-                              break;
+            case "rectangle": return drawRectangleFractal(900, 900, 150, 50, Color.blue, myCanvas, 5, 0, 0);
 
             default: System.out.println("this should not happen");
         }
@@ -34,11 +31,13 @@ public class FractalDrawer {
 
     //TODO:
     // drawTriangleFractal draws a triangle fractal using recursive techniques
-    public static void drawTriangleFractal(double width, double height, double x, double y, Color start, Canvas can, int level, int colorNum) {
+    public static double drawTriangleFractal(double width, double height, double x, double y, Color start, Canvas can, int level, int colorNum, double sum) {
         Color[] color = {Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.magenta};
 
         Triangle triangle = new Triangle(x, y, width, height);
         triangle.setColor(start);
+
+        sum = triangle.calculateArea();
 
         can.drawShape(triangle);
 
@@ -49,30 +48,76 @@ public class FractalDrawer {
 
             --level;
 
-            drawTriangleFractal(width/2, height/2, x, y, color[colorNum], can, level, colorNum);
-            drawTriangleFractal(width/2, height/2, x + width/2, y, color[colorNum], can, level, colorNum);
-            drawTriangleFractal(width/2, height/2, x + width/4, y - height/2, color[colorNum], can, level, colorNum);
+            sum += drawTriangleFractal(width/2, height/2, x, y, color[colorNum], can, level, colorNum, sum);
+            sum += drawTriangleFractal(width/2, height/2, x + width/2, y, color[colorNum], can, level, colorNum, sum);
+            sum += drawTriangleFractal(width/2, height/2, x + width/4, y - height/2, color[colorNum], can, level, colorNum, sum);
         }
+
+        return sum;
     }
 
 
     // TODO:
     // drawCircleFractal draws a circle fractal using recursive techniques
-    public static void drawCircleFractal(double radius, double x, double y, Color c, Canvas can, int level) {
+    public static double drawCircleFractal(double radius, double x, double y, Color start, Canvas can, int level, int colorNum, double sum) {
+        Color[] color = {Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.magenta};
+
         Circle circle = new Circle(x, y, radius);
-        circle.setColor(c);
+        circle.setColor(start);
+
+        sum = circle.calculateArea();
 
         can.drawShape(circle);
+
+        if (level > 0) {
+
+            if (colorNum >= 5) colorNum = 0;
+            else ++colorNum;
+
+            --level;
+
+            sum += drawCircleFractal(radius/2, x + radius/2, y, color[colorNum], can, level, colorNum, sum);
+            sum += drawCircleFractal(radius/2, x - radius/2, y, color[colorNum], can, level, colorNum, sum);
+            sum += drawCircleFractal(radius/3, x, y + (2*radius)/3, color[colorNum], can, level, colorNum, sum);
+            sum += drawCircleFractal(radius/3, x, y - (2*radius)/3, color[colorNum], can, level, colorNum, sum);
+        }
+
+        return sum;
     }
 
 
     //TODO:
     // drawRectangleFractal draws a rectangle fractal using recursive techniques
-    public static void drawRectangleFractal(double width, double height, double x, double y, Color c, Canvas can, int level) {
+    public static double drawRectangleFractal(double width, double height, double x, double y, Color start, Canvas can, int level, int colorNum, double sum) {
+        Color[] color = {Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.magenta};
+
         Rectangle rectangle = new Rectangle(x, y, width, height);
-        rectangle.setColor(c);
+        rectangle.setColor(start);
+
+        Rectangle rectangle2 = new Rectangle(x + width/3, y + height/3, width/3, height/3);
+        
+        sum = rectangle.calculateArea() + rectangle2.calculateArea();
         
         can.drawShape(rectangle);
+
+        if (level > 0) {
+
+            if (colorNum >= 5) colorNum = 0;
+            else ++colorNum;
+
+            --level;
+
+            sum += drawRectangleFractal(width/3, height/3, x, y + (2*height)/3, color[colorNum], can, level, colorNum, sum);
+            sum += drawRectangleFractal(width/3, height/3, x + width/3, y + (2*height)/3, color[colorNum], can, level, colorNum, sum);
+            sum += drawRectangleFractal(width/3, height/3, x + (2*width)/3, y + (2*height)/3, color[colorNum], can, level, colorNum, sum);
+            sum += drawRectangleFractal(width/3, height/3, x, y + height/3, color[colorNum], can, level, colorNum, sum);
+            sum += drawRectangleFractal(width/3, height/3, x + (2*width)/3, y + height/3, color[colorNum], can, level, colorNum, sum);
+            sum += drawRectangleFractal(width/3, height/3, x, y, color[colorNum], can, level, colorNum, sum);
+            sum += drawRectangleFractal(width/3, height/3, x + width/3, y, color[colorNum], can, level, colorNum, sum);
+            sum += drawRectangleFractal(width/3, height/3, x + (2*width)/3, y, color[colorNum], can, level, colorNum, sum);
+        }
+
+        return sum;
     }
 
     //TODO:
@@ -86,6 +131,8 @@ public class FractalDrawer {
         System.out.print("type triangle, circle, or rectangle: ");
 
         String shape = s.nextLine().toLowerCase();
-        drawFractal(shape);
+
+        System.out.println();
+        System.out.println("area of " + shape + " fractal: " + drawFractal(shape));
     }
 }
