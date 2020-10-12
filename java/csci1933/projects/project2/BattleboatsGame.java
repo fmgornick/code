09e3789083e01320f7d80c7ec88755397f483759
fmgornick <fletchermgornick@gmail.com>
2020-10-12 17:03:11 -0500

    project 2 more work

--- java/csci1933/projects/project2/BattleboatsGame.java
+++ java/csci1933/projects/project2/BattleboatsGame.java
@@ -15,8 +15,7 @@ public class BattleboatsGame {
         System.out.print("> ");
         String se = s.nextLine();
         
-        boolean tf = true;
-        while (tf == true) {
+        while (true) {
             if (se.equals("1")) {
                 game = new BattleboatsBoard(1);
                 game.placeBoats();
@@ -38,7 +37,7 @@ public class BattleboatsGame {
                 System.out.println("quit - end the game");
 
                 game.display();
-                tf = false;
+                break;
             }
 
             else if (se.equals("2")) {
@@ -62,17 +61,17 @@ public class BattleboatsGame {
                 System.out.println("quit - end the game");
 
                 game.display();
-                tf = false;
+                break;
             }
 
             else {
+                System.out.println();
                 System.out.println("invalid input. please enter 1 for standard or 2 for expert.");
                 System.out.print("> ");
                 se = s.nextLine();
             }
         }
 
-        game.print();
         System.out.println();
         System.out.print("> ");
         String c = s.nextLine();
@@ -83,43 +82,59 @@ public class BattleboatsGame {
                 System.out.print("coordinates: ");
                 c = s.nextLine();
 
-                int[] coordinates = new int[c.split(" ").length];
-                for (int i = 0; i < c.split(" ").length; i++) {
-                    coordinates[i] = Integer.parseInt(c.split(" ")[i]);
-                }
+                int[] coordinates;
 
-                if (coordinates.length == 2 && (coordinates[0] < 0 || coordinates[0] >= game.board.length || coordinates[1] < 0 || coordinates[1] >= game.board.length)) {
-                    System.out.println();
-                    System.out.println("penalty: index out of range, you lose a turn");
-                    shots++;
-                    turns++;
-                }
+                while (true) {
+                    coordinates = new int[c.split(" ").length];
+                    for (int i = 0; i < c.split(" ").length; i++) {
+                        coordinates[i] = Integer.parseInt(c.split(" ")[i]);
+                    }
 
-                else if (game.board[coordinates[0]][coordinates[1]].status == 'H' || game.board[coordinates[0]][coordinates[1]].status == 'M') {
-                    System.out.println();
-                    System.out.println("penalty: you already fired at this location, you lose a turn");
-                    shots++;
-                    turns++;
-                }
+                    if (coordinates.length != 2) {
+                        System.out.println();
+                        System.out.println("error: make sure you type two integers separated by a space");
+                        System.out.print("coordinates: ");
+                        c = s.nextLine();
 
-                else if (coordinates.length == 2 && coordinates[0] >= 0 && coordinates[0] < game.board.length && coordinates[1] >= 0 && coordinates[1] < game.board.length) {
-                    game.fire(coordinates[0], coordinates[1]);
-                    shots++;
+                    }
+                    else if (coordinates.length == 2 && (coordinates[0] < 0 || coordinates[0] >= game.board.length || coordinates[1] < 0 || coordinates[1] >= game.board.length)) {
+                        System.out.println();
+                        System.out.println("penalty: index out of range, you lose a turn");
+                        shots++;
+                        turns++;
+                        break;
+                    }
 
-                    if (game.board[coordinates[0]][coordinates[1]].status == 'H') {
-                        if (game.sunk(coordinates[0], coordinates[1])) {
-                            System.out.println();
-                            System.out.println("you sunk my battleship!");
+                    else if (game.board[coordinates[0]][coordinates[1]].status == 'H' || game.board[coordinates[0]][coordinates[1]].status == 'M') {
+                        System.out.println();
+                        System.out.println("penalty: you already fired at this location, you lose a turn");
+                        shots++;
+                        turns++;
+                        break;
+                    }
+
+                    else if (coordinates.length == 2 && coordinates[0] >= 0 && coordinates[0] < game.board.length && coordinates[1] >= 0 && coordinates[1] < game.board.length) {
+                        game.fire(coordinates[0], coordinates[1]);
+                        shots++;
+
+                        if (game.board[coordinates[0]][coordinates[1]].status == 'H') {
+                            if (game.sunk(coordinates[0], coordinates[1])) {
+                                System.out.println();
+                                System.out.println("you sunk my battleship!");
+                                break;
+                            }
+
+                            else {
+                                System.out.println();
+                                System.out.println("hit!");
+                                break;
+                            }
                         }
-
                         else {
                             System.out.println();
-                            System.out.println("hit!");
+                            System.out.println("miss");
+                            break;
                         }
-                    }
-                    else {
-                        System.out.println();
-                        System.out.println("miss");
                     }
                 }
 
@@ -135,7 +150,6 @@ public class BattleboatsGame {
                 if ((se.equals("1") && missile == 0) || (se.equals("2") && missile < 2)) {
                     System.out.print("coordinates: ");
                     c = s.nextLine();
-
                     int[] coordinates = new int[c.split(" ").length];
                     for (int i = 0; i < c.split(" ").length; i++) {
                         coordinates[i] = Integer.parseInt(c.split(" ")[i]);
@@ -143,7 +157,7 @@ public class BattleboatsGame {
 
                     if (coordinates.length == 2 && (coordinates[0] < 0 || coordinates[0] >= game.board.length || coordinates[1] < 0 || coordinates[1] >= game.board.length)) {
                         System.out.println();
-                        System.out.println("penalty: index out of range, you lose a turn (you can still use the missile on the next turn");
+                        System.out.println("penalty: index out of range, you lose a turn (you can still use the missile on the next turn)");
                         turns++;
                     }
 
@@ -169,6 +183,114 @@ public class BattleboatsGame {
                 System.out.print("> ");
                 c = s.nextLine();
             }
+
+            else if (c.equals("drone")) {
+
+                if ((se.equals("1") && drone == 0) || (se.equals("2") && drone < 2)) {
+                    System.out.println("row or column? (type \"r\" for row or \"c\" for column)");
+                    System.out.print("> ");
+                    c = s.nextLine();
+
+                    while (true) {
+                        if (c.equals("r")) {
+                            System.out.println();
+                            System.out.println("which row?");
+                            System.out.print("> ");
+
+                            c = s.nextLine();
+                            int row = Integer.parseInt(c);
+
+                            if (row >= 0 && ((se.equals("1") && row < 8) || (se.equals("2") && row < 12))) {
+                                System.out.println();
+                                System.out.println("there are " + game.drone(1, row) + " cell(s) containing boats in this row");
+                                drone++;
+                                break;
+                            }
+
+                            else { 
+                                System.out.println();
+                                System.out.println("penalty: index out of range, you lose a turn (you can still use the drone on the next turn)");
+                                turns++;
+                                break;
+                            }
+                        }
+
+                        else if (c.equals("c")) {
+                            System.out.println();
+                            System.out.println("which column?");
+                            System.out.print("> ");
+
+                            c = s.nextLine();
+                            int col = Integer.parseInt(c);
+
+                            if (col >= 0 && col < game.board.length) {
+                                System.out.println();
+                                System.out.println("there are " + game.drone(0, col) + " cell(s) containing boats in this column");
+                                drone++;
+                                break;
+                            }
+
+                            else { 
+                                System.out.println();
+                                System.out.println("penalty: index out of range, you lose a turn (you can still use the drone on the next turn)");
+                                turns++;
+                                break;
+                            }
+                        }
+
+                        else {
+                            System.out.println();
+                            System.out.println("invalid input, please type \"r\" for row or \"c\" for column)");
+                            System.out.print("> ");
+                            c = s.nextLine();
+                        }
+                    }
+                }
+
+                else if (se.equals("1") && drone > 0) {
+                    System.out.println();
+                    System.out.println("you already used the drone once, you only get one drone per game");
+                }
+
+                else {
+                    System.out.println();
+                    System.out.println("you already used the drone twice, you only get two drones per game");
+                }
+
+                turns++;
+                game.display();
+                System.out.println();
+                System.out.print("> ");
+                c = s.nextLine();
+            }
+
+            else if (c.equals("display")) {
+                game.display();
+                System.out.println();
+                System.out.print("> ");
+                c = s.nextLine();
+            }
+
+            else if (c.equals("print")) {
+                game.print();
+                System.out.println();
+                System.out.print("> ");
+                c = s.nextLine();
+            }
+
+
+            else {
+                System.out.println("make sure you typed the command correctly");
+                System.out.println();
+                System.out.println("COMMAND OPTIONS: \'fire\', \'missile\', \'drone\', \'display\', \'print\', \'quit\'");
+                System.out.print("> ");
+                c = s.nextLine();
+
+            }
         }
+
+        System.out.println();
+        System.out.println("number of shots fired (not including missiles): " + shots);
+        System.out.println("number of turns: " + turns);
     }
 }
