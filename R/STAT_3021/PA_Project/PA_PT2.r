d6b8e0224cd428ecec2cfee8b8f73a21613deeef
fmgornick <fletchermgornick@gmail.com>
2020-12-05 15:42:54 -0600

    ?

--- R/STAT_3021/PA_Project/PA_PT2.r
+++ R/STAT_3021/PA_Project/PA_PT2.r
@@ -1,39 +1,20 @@
 rap <- read.csv("rapData.csv", header = TRUE)[c("SongName", "PercentageDistinct")]
 country <- read.csv("countryData.csv", header = TRUE)[c("SongName", "PercentageDistinct")]
 
-cat("for this project we have two separate csv files that want to keep separate for now, until we compare in pt 3\n\n")
+r <- c(rap$PercentageDistinct)
+c <- c(country$PercentageDistinct)
 
-cat("RAP\n")
-names(rap)
 
-cat("head\n")
-head(rap)
-cat("\n")
-cat("tail\n")
-tail(rap)
+sd(r)
+sd(c)
 
-cat("\n\n")
+t <- (mean(r) - mean(c)) / sqrt(sd(r)^2/30 + sd(c)^2/30)
+v <- 29
 
-cat("COUNTRY\n")
-names(country)
-cat("head\n")
-head(country)
-cat("\n")
-cat("tail\n")
-tail(country)
+p <- 1 - pt(t,df=v)
 
-hist(rap$PercentageDistinct, main="Histogram for Percentage of Distinct Words in Rap Songs", xlab="Percentage", ylab="Number of Songs", xlim=c(15,45), ylim=c(0,10))
-hist(country$PercentageDistinct, main="Histogram for Percentage of Distinct Words in Country Songs", xlab="Percentage", ylab="Number of Songs")
-boxplot(rap$PercentageDistinct,country$PercentageDistinct,main="Boxplot of Distinct Word Percentages", names=c("Rap","Country"),ylab="Percentage")
+cat(t)
 
-cat("\n\n")
-
-summary(rap$PercentageDistinct)
-cat("\n")
-summary(country$PercentageDistinct)
-
-qqnorm(rap$PercentageDistinct, pch = 1, frame = FALSE, main="QQ Plot for Percentage of Distinct Words in Rap Songs")
-qqline(rap$PercentageDistinct, col="steelblue", lwd = 2)
-
-qqnorm(country$PercentageDistinct, pch = 1, frame = FALSE, main="QQ Plot for Percentage of Distinct Words in Country Songs")
-qqline(country$PercentageDistinct, col="steelblue", lwd = 2)
+t.test(r,c,alternative="two.sided",var.equal=FALSE,conf.level=0.95)
+sd(r)
+sd(c)
