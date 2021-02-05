4917e56c119b034c82112c6595a9c737d7eb187c
Fletcher Gornick <gorni025@umn.edu>
2021-02-05 01:25:30 -0600

    finished project 1

--- C/csci2021/projects/p1-code/hashset_funcs.c
+++ C/csci2021/projects/p1-code/hashset_funcs.c
@@ -1,39 +1,232 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 #include "hashset.h"
 
+// given
 long  hashcode(char key[]) {
-  return 0;
+  union {
+    char str[8];
+    long num;
+  } strnum;
+  strnum.num = 0;
+
+  for(int i=0; i<8; i++) {
+    if(key[i] == '\0'){
+      break;
+    }
+    strnum.str[i] = key[i];
+  }
+  return strnum.num;
 }
+
+// finds prime number
 int   next_prime(int num) {
-  return 0;
+  int prime = num;
+  int isPrime = 0;
+
+  while (!isPrime) {
+    // iterates from 2 to half the size of the number to cover every possible
+    // dividend
+    for (int i=2; i<=prime/2 && prime % i != 0; i++) {
+      // break condition for while loop
+      if (i == prime/2) isPrime = 1;
+    }
+    // increments if not prime
+    if (isPrime == 0) prime++;
+  }
+  return prime;
 }
 
+// initializes each field of hashset to null
 void  hashset_init(hashset_t *hs, int table_size) {
-  return;
-}
-int   hashset_add(hashset_t *hs, char item[]) {
-  return 0;
-}
-int   hashset_contains(hashset_t *hs, char key[]) {
-  return 0;
-}
-void  hashset_expand(hashset_t *hs) {
-  return;
-}
-void  hashset_free_fields(hashset_t *hs) {
-  return;
+  hs->item_count = 0;
+  hs->table_size = table_size;
+  hs->table = malloc(table_size * sizeof(hashnode_t));
+  // initializes each element in the table to NULL
+  for (int i=0; i<table_size; i++) hs->table[i] = NULL;
+  hs->order_first = NULL;
+  hs->order_last = NULL;
 }
 
-void  hashset_write_items_ordered(hashset_t *hs, FILE *out) {
-  return;
+// adds hashnode to hashset, return 1 if change was made
+int   hashset_add(hashset_t *hs, char item[]) {
+  // doesn't do anything if the node is already present
+  if (hashset_contains(hs,item)) return 0;
+  hashnode_t *node = malloc(sizeof(hashnode_t));
+  // copy string input into hashnode item field
+  strcpy(node->item,item);
+
+  // special case where hashnode is the first in the set
+  if (hs->item_count == 0) {
+    node->table_next = NULL;
+    node->order_next = NULL;
+
+    hs->item_count++;
+    hs->table[hashcode(item) % hs->table_size] = node;
+    hs->order_first = node;
+    hs->order_last = node;
+
+    return 1;
+  }
+
+  else {
+    // checks to see if bucket already contains a node, if not the current node
+    // will not have a table_next field
+    if (hs->table[hashcode(item) % hs->table_size] == NULL) node->table_next = NULL;
+    // if there is already a node in the bucket, then set that as the
+    // table_next field
+    else node->table_next = hs->table[hashcode(item) % hs->table_size];
+    node->order_next = NULL;
+
+    hs->item_count++;
+    hs->table[hashcode(item) % hs->table_size] = node;
+    hs->order_last->order_next = node;
+    hs->order_last = node;
+
+    return 1;
+  }
 }
-void  hashset_show_structure(hashset_t *hs) {
-  return;
-}
-void  hashset_save(hashset_t *hs, char *filename) {
-  return;
-}
-int   hashset_load(hashset_t *hs, char *filename) {
+
+// check if the hashset contains a node with an item field matching key
+int   hashset_contains(hashset_t *hs, char key[]) {
+  // point to the table index would be located with the hash function
+  hashnode_t *ptr = hs->table[hashcode(key) % hs->table_size];
+  // iterate through every element of the hash table index
+  while (ptr != NULL) {
+    // compare the item field with key and return one if they match
+    if (strcmp(ptr->item, key) == 0) return 1;
+    ptr = ptr->table_next;
+  }
   return 0;
 }
+
+// increase the size by the next prime number greater than 2 times it's
+// original size
+void  hashset_expand(hashset_t *hs) {
+  hashset_t temp;
+  // initialize hashset with new size
+  hashset_init(&temp,next_prime(2*hs->table_size+1));
+  hashnode_t *ptr = hs->order_first;
+
+  // add all the elements in the original set to the new one
+  while (ptr != NULL) {
+    hashset_add(&temp,ptr->item);
+    ptr = ptr->order_next;
+  }
+
+  // de-allocate all the memory associated with the old hashset
+  hashset_free_fields(hs);
+  *hs = temp;
+}
+
+// function used to free all the hashnode memory and that hashset's table
+// memory.  Also initializes all the hashset fields to 0 or NULL
+void  hashset_free_fields(hashset_t *hs) {
+  hs->item_count = 0;
+  hs->table_size = 0;
+  hashnode_t *head = hs->order_first;
+  hashnode_t *tail = hs->order_first;
+  // iterate through hashnodes and free them
+  while (head != NULL) {
+    head = head->order_next;
+    free(tail);
+    tail = head;
+  }
+  hs->order_first = NULL;
+  hs->order_last = NULL;
+  // free the empty table
+  free(hs->table);
+}
+
+// write all the hashnodes in the hashset to a file in the order they were
+// added
+void  hashset_write_items_ordered(hashset_t *hs, FILE *out) {
+  hashnode_t *ptr = hs->order_first;
+  int i = 1;
+  // iterate through nodes until ptr reached them all
+  while (ptr != NULL) {
+    // write a line with the node information into the file
+    fprintf(out,"%3d %s\n",i,ptr->item);
+    ptr = ptr->order_next;
+    i++;
+  }
+}
+
+// prints out all the details of each field in the hashset as well as a visual
+// representation of the hashset table with all the hashnodes
+void  hashset_show_structure(hashset_t *hs) {
+  printf("item_count: %d\n",hs->item_count);                    //item_count
+  printf("table_size: %d\n",hs->table_size);                    //table_size
+  if (hs->order_first == NULL) printf("order_first: NULL\n");   //order_first
+  else printf("order_first: %s\n",hs->order_first->item);
+  if (hs->order_last == NULL) printf("order_last : NULL\n");    //order_last
+  else printf("order_last : %s\n",hs->order_last->item);
+  printf("load_factor: %.4f\n",((float) hs->item_count) / ((float) hs->table_size)); //load_factor
+
+  hashnode_t *ptr;
+  char next[128];
+  // iterate through hash table printing nodes in each index
+  for (int i=0; i<hs->table_size; i++) {
+    printf("[%2d] : ",i);
+    ptr = hs->table[i];
+    // print all the nodes in each index
+    while (ptr != NULL) {
+      if (ptr->order_next == NULL) strcpy(next,"NULL");
+      else strcpy(next,ptr->order_next->item);
+      printf("{%ld %s >>%s} ",hashcode(ptr->item),ptr->item,next);
+      ptr = ptr->table_next;
+    }
+    printf("\n");
+    
+  }
+}
+
+// puts the hashset size and item count in the file and uses the
+// hashset_write_items_ordered function to print the rest of the node info
+void  hashset_save(hashset_t *hs, char *filename) {
+  FILE *f = fopen(filename,"w");
+  // exits if file cannot be opened
+  if (f == NULL) {
+    printf("ERROR: could not open file '%s'\n",filename);
+    return;
+  }
+  // write in the table size and item count
+  fprintf(f,"%d %d\n",hs->table_size,hs->item_count);
+  // write in the rest of the information
+  hashset_write_items_ordered(hs,f);
+  fclose(f);
+}
+
+// takes a file and loads it's information into a new hashset, deleting all the
+// previous information
+int   hashset_load(hashset_t *hs, char *filename) {
+  FILE *f = fopen(filename,"r");
+  // exits if file cannot be opened
+  if (f == NULL) {
+    printf("ERROR: could not open file '%s'\n",filename);
+    return 0;
+  }
+
+  // frees up all the memory in the hashset to put in new values
+  hashset_free_fields(hs);
+  int table_size;
+  int item_count;
+  int item_num;
+  char name[128];
+  // reads the first line of the file to get the size and count fields for the
+  // hashset
+  fscanf(f,"%d %d\n",&table_size,&item_count);
+  hashset_init(hs,table_size);
+
+  // iterates through each line and adds a new node with the given information
+  // (basically just the name)
+  for (int i=0; i<item_count; i++) {
+    fscanf(f,"%3d %s\n",&item_num,name);
+    hashset_add(hs,name);
+  }
+  fclose(f);
+  return 1;
+}
+
--- C/csci2021/projects/p1-code/hashset_main.c
+++ C/csci2021/projects/p1-code/hashset_main.c
@@ -1,3 +1,156 @@
+// I basically copied this outline from lab 2, but that's what you suggested
+// so I figured I would cite my source
+#include <stdio.h>
+#include <string.h>
+#include "hashset.h"
+
 int main(int argc, char *argv[]) {
+  int echo = 0;
+  // check if this is run with the 'echo' command or just through stdin
+  if (argc > 1 && strcmp("-echo",argv[1]) == 0) echo = 1;
+
+  // prints all the commands that you can input
+  printf("Hashset Application\n");
+  printf("Commands:\n");
+  printf("  hashcode <item>  : prints out the numeric hash code for the given key (does not change the hash set)\n");
+  printf("  contains <item>  : prints the value associated with the given item or NOT PRESENT\n");
+  printf("  add <item>       : inserts the given item into the hash set, reports existing items\n");
+  printf("  print            : prints all items in the hash set in the order they were addded\n");
+  printf("  structure        : prints detailed structure of the hash set\n");
+  printf("  clear            : reinitializes hash set to be empty with default size\n");
+  printf("  save <file>      : writes the contents of the hash set to the given file\n");
+  printf("  load <file>      : clears the current hash set and loads the one in the given file\n");
+  printf("  next_prime <int> : if <int> is prime, prints it, otherwise finds the next prime and prints it\n");
+  printf("  expand           : expands memory size of hash set to reduce its load factor\n");
+  printf("  bye              : exit the program\n");
+
+  // gets assigned to user input
+  char cmd[128];
+  hashset_t hs;
+  // 1 if function ran properly or 0 otherwise
+  int success;
+  // initialize hashset to default size (5)
+  hashset_init(&hs, HASHSET_DEFAULT_TABLE_SIZE);
+
+  // referring to the if (echo) statements, it just prints to the screen what
+  // was in the file that was used to run a test
+  while (1) {
+    // application prompt
+    printf("HS|> ");
+    // check to see if user exited
+    success = fscanf(stdin,"%s",cmd);
+    if (success == EOF) {
+      printf("\n");
+      break;
+    }
+
+    // runs hashcode command
+    if (strcmp("hashcode",cmd) == 0) {
+      fscanf(stdin,"%s",cmd);
+      if (echo) printf("hashcode %s\n",cmd);
+      printf("%ld\n",hashcode(cmd));
+    }
+
+    // runs hashset_contains command
+    else if (strcmp("contains",cmd) == 0) {
+      fscanf(stdin,"%s",cmd);
+      if (echo) printf("contains %s\n",cmd);
+
+      success = hashset_contains(&hs,cmd);
+      // says it found the string if contains evaluates to 1
+      if (success) printf("FOUND: %s\n",cmd);
+      // says it's not present otherwise
+      else printf("NOT PRESENT\n");
+    }
+
+    // runs hashset_add function
+    else if (strcmp("add",cmd) == 0) {
+      fscanf(stdin,"%s",cmd);
+      if (echo) printf("add %s\n",cmd);
+
+      success = hashset_add(&hs,cmd);
+      // if item is already in hashset you will be notified
+      if (!success) printf("Item already present, no changes made\n");
+    }
+
+    // no print command in hashset_funcs.c so I just implemented a short one
+    // here (I probably could have use hashset_write_items_ordered but idc
+    else if (strcmp("print",cmd) == 0) {
+      if (echo) printf("print\n");
+      // point to first node in hashset
+      hashnode_t *ptr = hs.order_first;
+      int i = 1;
+      // iterate through numbering them all off and printing result to screen
+      while (ptr != NULL) {
+        printf("%3d %s\n",i,ptr->item);
+        ptr = ptr->order_next;
+        i++;
+      }
+    }
+
+    // runs hashset_show_structure command
+    else if (strcmp("structure",cmd) == 0) {
+      if (echo) printf("structure\n");
+      // this will print a ton of information to the screen described in
+      // hashset_funcs
+      hashset_show_structure(&hs);
+    }
+
+    // clears up all the memory and re-initializes it
+    else if (strcmp("clear",cmd) == 0) {
+      if (echo) printf("clear\n");
+      // free memory
+      hashset_free_fields(&hs);
+      // re-initialize
+      hashset_init(&hs,HASHSET_DEFAULT_TABLE_SIZE);
+    }
+
+    // runs the hashset_save command
+    else if (strcmp("save",cmd) == 0) {
+      fscanf(stdin,"%s",cmd);
+      if (echo) printf("save %s\n",cmd);
+      hashset_save(&hs,cmd);
+    }
+
+    // runs the hashset_load command
+    else if (strcmp("load",cmd) == 0) {
+      fscanf(stdin,"%s",cmd);
+      if (echo) printf("load %s\n",cmd);
+      success = hashset_load(&hs,cmd);
+      // if file could not be loaded, print failure to the screen
+      if (!success) printf("load failed\n");
+    }
+
+    // finds the next prime after an inputed number
+    else if (strcmp("next_prime",cmd) == 0) {
+      // using num instead of cmd because cmd is a string
+      int num;
+      fscanf(stdin,"%d",&num);
+      if (echo) printf("next_prime %d\n",num);
+      printf("%d\n",next_prime(num));
+    }
+
+    // runs the hashset_expand command
+    else if (strcmp("expand",cmd) == 0) {
+      if (echo) printf("expand\n");
+      hashset_expand(&hs);
+    }
+
+    // exits the program
+    else if (strcmp("bye",cmd) == 0) {
+      if (echo) printf("bye\n");
+      break;
+    }
+
+    // notifies you if you misspelled something
+    else {
+      if (echo) printf("%s\n",cmd);
+      printf("unknown command %s\n",cmd);
+    }
+
+  }
+
+  // free all the remaining memory
+  hashset_free_fields(&hs);
   return 0;
 }
