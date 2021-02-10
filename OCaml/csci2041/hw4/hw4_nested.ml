5e787c843c3718714f15e26efcbd5a63b2ed3087
Fletcher Gornick <gorni025@umn.edu>
2021-02-10 15:55:27 -0600

    2041 bonus solved

--- OCaml/csci2041/hw4/hw4_nested.ml
+++ OCaml/csci2041/hw4/hw4_nested.ml
@@ -1,11 +1,15 @@
 type 'a nested_list = Scalar of 'a | Nested of 'a nested_list list
 
-let flatten : 'a . 'a nested_list -> 'a list =
-  fun l ->
-    if l = Scalar _ then [l]
-    else fh l
+let rec append : 'a . 'a list -> 'a list -> 'a list =
+    fun l1 l2 ->
+        match l1 with
+        | [] -> l2
+        | h1 :: t1 -> h1 :: append t1 l2
 
-let rec fh : 'a . 'a nested_list -> 'a list =
+let rec flatten : 'a . 'a nested_list -> 'a list =
   fun l ->
-    match l with
-    | Scalar x -> x
+      match l with
+      | Scalar x -> [x]
+      | Nested [] -> []
+      | Nested (h::t) -> append (flatten h) (flatten (Nested t))
+
