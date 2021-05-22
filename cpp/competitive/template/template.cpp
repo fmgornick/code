3fa1f2825c37ed5ab72b5d32e544bbd2e7425ac0
Fletcher Gornick <fletchermgornick@gmail.com>
2021-05-21 22:51:02 -0500

    mmhm

---
+++ cpp/competitive/template/template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define F first;
+#define S second;
+#define PB push_back;
+#define MP make_pair;
+
+int main() {
+    int a, b;
+    string c;
+    cout << "input 2 numbers and a word...\n";
+    cin >> a >> b;
+    getline(cin, c);
+    cout << "\nnum 1: " << a << "\n"
+    << "num 2: " << b << "\n"
+    << "word: " << c << endl;
+    return 0;
+}
