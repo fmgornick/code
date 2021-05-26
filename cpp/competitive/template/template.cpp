01319029bf9d43b7e66850fb78b9c3b613be742a
Fletcher Gornick <fletchermgornick@gmail.com>
2021-05-25 22:18:30 -0500

    changed template file

--- cpp/competitive/template/template.cpp
+++ cpp/competitive/template/template.cpp
@@ -11,13 +11,5 @@ typedef pair<int,int> pi;
 #define MP make_pair;
 
 int main() {
-    int a, b;
-    string c;
-    cout << "input 2 numbers and a word...\n";
-    cin >> a >> b;
-    getline(cin, c);
-    cout << "\nnum 1: " << a << "\n"
-    << "num 2: " << b << "\n"
-    << "word: " << c << endl;
     return 0;
 }
