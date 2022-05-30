21e215b56d39cd2902bd6f2f36c36d317db19ac9
Fletcher Gornick <fletchermgornick@gmail.com>
2022-05-30 18:59:48 -0500

    cleaned up guessing game too

--- rust/book/ch2/guessing_game/target/debug/build/libc-c22666c69cc729af/build_script_build-c22666c69cc729af.d
+++
@@ -1,5 +0,0 @@
-/Users/Fletcher/code/rust/book/ch2/guessing_game/target/debug/build/libc-c22666c69cc729af/build_script_build-c22666c69cc729af: /Users/Fletcher/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.94/build.rs
-
-/Users/Fletcher/code/rust/book/ch2/guessing_game/target/debug/build/libc-c22666c69cc729af/build_script_build-c22666c69cc729af.d: /Users/Fletcher/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.94/build.rs
-
-/Users/Fletcher/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.94/build.rs:
