397f0d986c60c438d304897f543c46076643ea42
Fletcher Gornick <fletchermgornick@gmail.com>
2022-05-30 18:58:59 -0500

    cleaned up rust project

--- rust/book/ch4/first_word/target/debug/deps/first_word-495f57b2422b46c7.d
+++
@@ -1,5 +0,0 @@
-/Users/Fletcher/code/rust/book/ch4/first_word/target/debug/deps/first_word-495f57b2422b46c7.rmeta: src/main.rs
-
-/Users/Fletcher/code/rust/book/ch4/first_word/target/debug/deps/first_word-495f57b2422b46c7.d: src/main.rs
-
-src/main.rs:
--- rust/book/ch4/first_word/target/debug/deps/first_word-4d963db7b30de971.d
+++
@@ -1,5 +0,0 @@
-/Users/Fletcher/code/rust/book/ch4/first_word/target/debug/deps/first_word-4d963db7b30de971.rmeta: src/main.rs
-
-/Users/Fletcher/code/rust/book/ch4/first_word/target/debug/deps/first_word-4d963db7b30de971.d: src/main.rs
-
-src/main.rs:
