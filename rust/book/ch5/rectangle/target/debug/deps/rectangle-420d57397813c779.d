0163fc604beb7f800045ded1ac2d98c0c0efb6d5
Fletcher Gornick <fletchermgornick@gmail.com>
2022-06-05 13:57:49 -0500

    cleaned container

--- rust/book/ch5/rectangle/target/debug/deps/rectangle-420d57397813c779.d
+++
@@ -1,5 +0,0 @@
-/Users/Fletcher/code/rust/book/ch5/rectangle/target/debug/deps/rectangle-420d57397813c779.rmeta: src/main.rs
-
-/Users/Fletcher/code/rust/book/ch5/rectangle/target/debug/deps/rectangle-420d57397813c779.d: src/main.rs
-
-src/main.rs:
--- rust/book/ch5/rectangle/target/debug/deps/rectangle-814686f4f2cd3d38.d
+++
@@ -1,5 +0,0 @@
-/Users/Fletcher/code/rust/book/ch5/rectangle/target/debug/deps/rectangle-814686f4f2cd3d38.rmeta: src/main.rs
-
-/Users/Fletcher/code/rust/book/ch5/rectangle/target/debug/deps/rectangle-814686f4f2cd3d38.d: src/main.rs
-
-src/main.rs:
