c4db42845be99a83833da2283f461177bc5fd92c
Fletcher Gornick <fletchermgornick@gmail.com>
2022-06-21 00:02:25 -0500

    day03a advent

--- rust/advent/day03a/src/main.rs
+++ rust/advent/day03a/src/main.rs
@@ -1,7 +1,20 @@
 fn main() {
-    let gamma = include_str!("../input.txt")
+    let count: usize = include_str!("../input.txt").lines().count();
+    let width: usize = include_str!("../input.txt").lines().next().unwrap().len();
+
+    let gamma: u32 = include_str!("../input.txt")
         .lines()
-        .map(|l| u32::from_str_radix(l, 2).expect("not binary"))
-        .fold(vec![(0,0); 5], |(count0, count1), num| {})
-    println!("Hello, world!");
+        .map(|l| usize::from_str_radix(l, 2).unwrap())
+        .fold(vec![0; width], |cnt, bits| {
+            cnt.into_iter()
+                .enumerate()
+                .map(|(i, n)| n + ((bits & 1 << i) >> i))
+                .collect()
+        })
+        .into_iter()
+        .enumerate()
+        .map(|(i, b)| if b >= count / 2 { 1 << i } else { 0 })
+        .sum();
+
+    println!("{}", gamma * (!gamma & (1 << width) - 1));
 }
